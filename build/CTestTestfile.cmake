c7a89774436c213ec75cb938a296bb07636ba4db
winrid <winrid@gmail.com>
2025-11-07 18:40:46 -0800

    Remove build artifacts from version control
    Build artifacts (binary .o files and CMake build files) were accidentally
    committed to the repository. This commit removes them from tracking.
    
    The .gitignore file already has the correct rules to prevent this from
    happening again.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/winrid/dev/fastcomments/fastcomments-cpp
-# Build directory: /home/winrid/dev/fastcomments/fastcomments-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("client")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/winrid/dev/fastcomments/fastcomments-cpp/tests
-# Build directory: /home/winrid/dev/fastcomments/fastcomments-cpp/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/winrid/dev/fastcomments/fastcomments-cpp/build/tests/sso_test[1]_include.cmake")
-include("/home/winrid/dev/fastcomments/fastcomments-cpp/build/tests/sso_integration_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
